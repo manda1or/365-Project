5bb61b5bb043a7f2b760a779e925ad1f370eb357
manda1or <102929138+manda1or@users.noreply.github.com>
2024-11-11 14:08:58 -0600

    reviews.go updated and working. Implemented in main.go and will display review after input is finished.

--- src/main/main.go
+++ src/main/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	//"fmt"
+	"365-Project/src/reviews"
 	"365-Project/src/tasks"
+	"fmt"
 )
 
 func main() {
 
 	//Task Manager instance
 	tm := tasks.TaskManager{}
-
 	// Task Creation
 	tm.CreateNewTask()
-
 	// Displays All Tasks
 	tm.ShowTasks()
+
+	// Review Code
+	review := reviews.CreateNewReview()
+	fmt.Println("Review List:")
+	fmt.Printf("Name: %s\n", review.ReviewerName)
+	fmt.Printf("Comment: %s\n", review.ReviewComment)
+	fmt.Printf("Rating: %d\n", review.ReviewRating)
 }
--- src/reviews/reviews.go
+++ src/reviews/reviews.go
@@ -1,7 +1,54 @@
 package reviews
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type ReviewStruct struct {
 	ReviewerName  string
 	ReviewComment string
 	ReviewRating  int
+}
+
+func CreateNewReview() ReviewStruct {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Make Your Review!")
+
+	// Reviewer Name Input
+	fmt.Println("Enter Your Name:")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	//Reviewer Comment Input
+	fmt.Println("Enter Your Comment:")
+	comment, _ := reader.ReadString('\n')
+	comment = strings.TrimSpace(comment)
+
+	// Reviewer Rating
+	var rating int
+	for {
+		fmt.Println("Enter a rating 1-5:")
+		ratingInput, _ := reader.ReadString('\n')
+		ratingInput = strings.TrimSpace(ratingInput)
+
+		//Turn string input into int
+		var err error
+		rating, err = strconv.Atoi(ratingInput)
+
+		if err != nil || rating < 1 || rating > 5 {
+			fmt.Println("Invalid input. Enter a valid rating (1-5).")
+			continue
+		}
+		break
+	}
+
+	return ReviewStruct{
+		ReviewerName:  name,
+		ReviewComment: comment,
+		ReviewRating:  rating,
+	}
 }
